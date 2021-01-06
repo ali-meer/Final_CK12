375be554486ae8566b35ce435fef1f2d523b6843
mzakkar99 <mzakkar@gmail.com>
2021-01-06 13:24:11 +0000

    2nd commit 06 jan

--- src/test/java/Util/Base.java
+++ src/test/java/Util/Base.java
@@ -5,5 +5,6 @@ import org.openqa.selenium.WebDriver;
 public class Base {
     public static WebDriver driver;
     private static String str;
-    private static String asac;
+    private  static String xyz;
+
 }
