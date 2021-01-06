c9adebb2f866a1a37cf36e751cc841731ac0ef67
Ali Meer <alimeer.testsoft@gmail.com>
2021-01-06 19:45:35 +0000

    Ali's first commit

--- src/test/java/Util/Base.java
+++ src/test/java/Util/Base.java
@@ -1,10 +1,24 @@
 package Util;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Base {
     public static WebDriver driver;
-    private static String str;
-    private  static String xyz;
+
+    public WebDriver launchBrowser(String browserName, String baseURL) throws InterruptedException {
+        if(browserName.equalsIgnoreCase("Chrome")){
+            System.setProperty("webdriver.chrome.driver",".\\drivers\\chromedriver.exe");
+            driver = new ChromeDriver();
+        }
+        Thread.sleep(1000);
+        driver.manage().window().maximize();
+        Thread.sleep(1000);
+        driver.manage().deleteAllCookies();
+        Thread.sleep(1000);
+        driver.get(baseURL);
+
+        return driver;
+    }
 
 }
